79ecd95b05029445362ebff4f2400c217960cf72
Rolem Galvez <rolemgalvez@gmail.com>
2023-01-23 09:05:57 +0800

    Add project files.

---
+++ EmployeeNames/Program.cs
@@ -0,0 +1,21 @@
+﻿//Welcome message
+Console.WriteLine("Employee Names");
+Console.WriteLine("--------------");
+
+//Prepare values
+List<string> names = new();
+names.Add("Juan");
+names.Add("Pedro");
+names.Add("Susan");
+names.Add("Manny");
+
+//Show values
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+//End of program
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
