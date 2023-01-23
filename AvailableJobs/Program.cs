1cd85fd19fe08386ab8ddb9a44fe4482373f29c8
Rolem Galvez <rolemgalvez@gmail.com>
2023-01-23 09:13:59 +0800

    feature: add projects AvailableJobs and MonthlyExpense

---
+++ AvailableJobs/Program.cs
@@ -0,0 +1,22 @@
+﻿//Welcome message
+Console.WriteLine("Available Jobs");
+Console.WriteLine("--------------");
+Console.WriteLine();
+
+//Prepare values
+List<string> jobs = new();
+jobs.Add("Technical Support");
+jobs.Add("Function Consultatnt");
+jobs.Add("Software Developer");
+jobs.Add("Project Manager");
+
+//Show values
+for (int i = 0; i < jobs.Count; i++)
+{
+    Console.WriteLine(jobs[i]);
+}
+
+//End of program
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
---
+++ MonthlyExpense/Program.cs
@@ -0,0 +1,28 @@
+﻿//Welcome message
+Console.WriteLine("Monthly Expense");
+Console.WriteLine("---------------");
+Console.WriteLine();
+
+//Prepare values
+List<decimal> expenses = new();
+expenses.Add(123.34M);
+expenses.Add(34234M);
+expenses.Add(34.23M);
+expenses.Add(324M);
+expenses.Add(348230948.123213M);
+
+decimal total = 0;
+
+//Sum expenses
+for (int i = 0; i < expenses.Count; i++)
+{
+    total += expenses[i];
+}
+
+//Show total
+Console.WriteLine($"Total monthly expense is Php {total.ToString("N")}");
+
+//End of program
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
