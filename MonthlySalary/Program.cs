7275c81d49d31537665731b81e8555a0ae180fc8
Rolem Galvez <rolemgalvez@gmail.com>
2023-01-24 11:10:43 +0800

    feature: add MonthlySalary and YearlyContribution projects

---
+++ MonthlySalary/Program.cs
@@ -0,0 +1,26 @@
+﻿//Welcome message
+Console.WriteLine("Monthly Salary");
+Console.WriteLine("--------------");
+Console.WriteLine();
+
+//Prepare values
+List<decimal> salaries = new();
+salaries.Add(5000.0M);
+salaries.Add(4000.123M);
+salaries.Add(6000.1M);
+salaries.Add(5050.50M);
+
+decimal monthlySalary = 0;
+
+for (int i = 0; i < salaries.Count; i++)
+{
+    monthlySalary += salaries[i];
+}
+
+//Show value
+Console.WriteLine($"Monthly salary is Php {monthlySalary.ToString("N")}");
+
+//End of program
+Console.WriteLine();
+Console.WriteLine("End of program");
+Console.ReadLine();
---
+++ YearlyContribution/Program.cs
@@ -0,0 +1,27 @@
+﻿//Welcome message
+Console.WriteLine("Yearly Contribution");
+Console.WriteLine("-------------------");
+Console.WriteLine();
+
+//Prepare value
+List<decimal> contributions = new();
+contributions.Add(123.45M);
+contributions.Add(234.56M);
+contributions.Add(345.67M);
+contributions.Add(456.78M);
+contributions.Add(567.89M);
+
+decimal yearlyContribution = 0;
+
+for (int i = 0; i < contributions.Count; i++)
+{
+    yearlyContribution += contributions[i];
+}
+
+//Show value
+Console.WriteLine($"Yearly contribution is Php {yearlyContribution.ToString("N")}");
+
+//End
+Console.WriteLine();
+Console.WriteLine("End of program.");
+Console.ReadLine();
